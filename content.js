f920e97b22a286b34b4ccb47cd43981020f99fd1
Jacob <jacobmars05@use.startmail.com>
2025-06-25 01:34:54 +1000

    added ljustxashton

--- content.js
+++ content.js
@@ -3,6 +3,7 @@ console.log("Bullshit Blocker Running");
 // List of blocked channels: add the channel handle (without '@') in lowercase.
 const blockedChannels = 
 [
+    "justxashton",
     "sabinehossenfelder", 
     "subboorahmadabbasi",
     "thunderboltsproject",
